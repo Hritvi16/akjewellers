1474f852ee2e2c1f0c7d2e041eff8c5c7646251f
Zeus <100012314+SSWEBCREATION344@users.noreply.github.com>
2022-05-06 18:09:34 +0530

    FINAL RELEASE

--- lib/api/Environment.dart
+++ lib/api/Environment.dart
@@ -1,6 +1,7 @@
 class Environment {
-  static String   url = "http://digitalwebgurukul.com/";
-  static String api = "akj_jewellery/";
+  //static String   url = "http://digitalwebgurukul.com/";
+  static String   url = "https://akjewellers.net/";
+  static String api = "apps/";
   static String media = "uploads/";
   static String imageUrl = url + media;
   static String rupee = "₹";
